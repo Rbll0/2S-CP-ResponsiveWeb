109c72e6768b610a7af45e6c6f4d46f762ead5e0
Gustavo Rabelo <gurabelo09@gmail.com>
2024-03-14 15:21:45 -0300

    criando projeto

---
+++ src/pages/Hamburgers/Hamburgers.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import { Button, CategoryList, Layout, ProductCard } from "../../components";
+import { ProductCategories, ProductWrapper } from "./Hamburgers.style";
+import { burgers, categoriesList } from "../../mocks";
+import {
+  ProductCardContent,
+  ProductCardPrice,
+} from "../../components/ProductCard/ProductCard.style";
+
+export default function Hamburgers() {
+  const [isLoading, setIsloading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const priceFormat = (price: number) => {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price);
+  };
+
+  const getCategories = async () => {
+    const url = "http://localhost:8000/categories"
+    setIsloading(true);
+    try {
+      const response = await fetch(url)
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log('cabô');
+      setIsloading(false);
+    }
+  };
+
+  const getHamburgers = async () => {
+    const url = "http://localhost:8000/hamburgers"
+    setIsloading(true)
+    setIsloading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+
+    } finally {
+      console.log('finnaly');
+      setIsloading(false);
+      setIsloading(false);
+
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    getHamburgers();
+  }, []);
+
+  return (
+    <Layout>
+      <h1>Hamburgers</h1>
+      <ProductCategories>
+        {isLoading ? (<p>Carregando</p>)
+          : (
+            categories.map((item, index) => (
+              <CategoryList key={index} data={item} />
+            ))
+          )}
+      </ProductCategories>
+      <ProductWrapper>
+        {isLoading
+          ? (<p>Carregando</p>)
+          : (
+            products.map((product, index) => (
+              <ProductCard key={index}>
+                <ProductCardContent>
+                  <h2>{product.title}</h2>
+                  <p>{product.description}</p>
+                  <Button onClick={() => { }}>Adicionar</Button>
+                </ProductCardContent>
+                <ProductCardPrice>
+                  {priceFormat(product.values.single)}
+                </ProductCardPrice>
+                <img src={product.image[0]} alt={product.title} />
+              </ProductCard>
+            ))
+          )
+        }
+      </ProductWrapper>
+    </Layout>
+  );
+}
