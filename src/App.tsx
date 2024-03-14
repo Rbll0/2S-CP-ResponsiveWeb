27ce21c489e1d3ebc0a0d020b6b5da7512dc5d25
Gustavo Rabelo <gurabelo09@gmail.com>
2024-03-14 20:59:02 -0300

    criando page bebidas e add route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 import Hamburgers from "./pages/Hamburgers/Hamburgers";
 import Appetizers from "./pages/Entradinhas/Entradinhas";
 import Desserts from "./pages/Sobremesas/Sobremesas";
+import Beverages from "./pages/Bebidas/Bebidas";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/hamburgers" element={<Hamburgers />} />
         <Route path="/entrada" element={<Appetizers />} />
         <Route path="/sobremesas" element={<Desserts />} />
+        <Route path="/bebidas" element={<Beverages />} />
       </Routes>
     </BrowserRouter>
   );
